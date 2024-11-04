b89089007e585349df5d4b29728b8a071aef525a
Тодор Христов <todorhristov2006@gmail.com>
2024-11-04 22:18:48 +0200

    Update ex.8

--- Python-Basics/Homework/01_First_Steps_In_Coding_Exercise/08_Basketball_Equipment/basketball_equipment.py
+++ Python-Basics/Homework/01_First_Steps_In_Coding_Exercise/08_Basketball_Equipment/basketball_equipment.py
@@ -1,15 +1,15 @@
 BASKETBALL_SNEAKERS = 0.40
-BASKETBALL_SHOES = 0.20
+BASKETBALL_CLOTHES = 0.20
 BASKETBALL_BALL = 0.25
 BASKETBALL_ACCESSORIES = 0.20
 
 yearly_tax = float(input())
 
-price_sneakers = yearly_tax * BASKETBALL_SNEAKERS
-price_shoes = yearly_tax * BASKETBALL_SHOES
-price_ball = yearly_tax * BASKETBALL_BALL
-price_accessories = yearly_tax * BASKETBALL_ACCESSORIES
+price_sneakers = yearly_tax - (yearly_tax * BASKETBALL_SNEAKERS)
+price_clothes = price_sneakers - (price_sneakers * BASKETBALL_CLOTHES)
+price_ball = price_clothes * BASKETBALL_BALL
+price_accessories = price_ball * BASKETBALL_ACCESSORIES
 
-total_price = yearly_tax + price_sneakers + price_shoes + price_ball + price_accessories
+total_price = yearly_tax + price_sneakers + price_clothes + price_ball + price_accessories
 
 print(total_price)
